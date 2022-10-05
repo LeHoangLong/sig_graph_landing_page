af04d9b29eb71bea8bb1cadd7b11fc9b9e8d25b1
Le Hoang Long <le.hoang.long@outlook.com>
2022-10-05 16:49:23 +0800

    Adding google analytics

--- wordpress/wp-content/themes/blockchain-lite/header.php
+++ wordpress/wp-content/themes/blockchain-lite/header.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-2ZC3ZBS1HP"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-2ZC3ZBS1HP');
+	</script>
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
